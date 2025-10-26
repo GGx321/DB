d909c6b764e4df56b4bf2c2a5fd44d2037a8e6b3
alex <@Gx321>
2025-10-26 17:30:35 +0100

    feat: add @nestjs/config module for environment configuration support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { ChatModule } from "./chat/chat.module";
 import { PushModule } from "./push/push.module";
-
+import { ConfigModule } from "@nestjs/config";
 @Module({
-  imports: [AuthModule, ChatModule, PushModule],
+  imports: [
+    AuthModule,
+    ChatModule,
+    PushModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+  ],
 })
 export class AppModule {}
