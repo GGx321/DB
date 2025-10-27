9c394a9fd627a1a1dc2edd203fb3b3c20228e5bd
alex <@Gx321>
2025-10-27 17:14:46 +0100

    refactor: comment out SSL certificate and private key handling in main.ts for future implementation

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,28 @@ import { NestApplicationOptions, ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import * as fs from "fs";
 
-function fileExists(filePath: string): boolean {
-  try {
-    fs.accessSync(filePath, fs.constants.F_OK);
-    console.log(`[OK] File exists: ${filePath}`);
-    return true;
-  } catch {
-    console.error(`[MISSING] File not found: ${filePath}`);
-    return false;
-  }
-}
+// function fileExists(filePath: string): boolean {
+//   try {
+//     fs.accessSync(filePath, fs.constants.F_OK);
+//     console.log(`[OK] File exists: ${filePath}`);
+//     return true;
+//   } catch {
+//     console.error(`[MISSING] File not found: ${filePath}`);
+//     return false;
+//   }
+// }
 
-const PRIVATE_KEY_PATH = process.env.SSL_PRIVATE_KEY;
-const PUBLIC_CERTIFICATE_PATH = process.env.SSL_PUBLIC_CERTIFICATE;
+// const PRIVATE_KEY_PATH = process.env.SSL_PRIVATE_KEY;
+// const PUBLIC_CERTIFICATE_PATH = process.env.SSL_PUBLIC_CERTIFICATE;
 
 async function bootstrap() {
   const options: NestApplicationOptions = {};
-  if (fileExists(PRIVATE_KEY_PATH) && fileExists(PUBLIC_CERTIFICATE_PATH)) {
-    options.httpsOptions = {
-      key: fs.readFileSync(PRIVATE_KEY_PATH),
-      cert: fs.readFileSync(PUBLIC_CERTIFICATE_PATH),
-    };
-  }
+  // if (fileExists(PRIVATE_KEY_PATH) && fileExists(PUBLIC_CERTIFICATE_PATH)) {
+  //   options.httpsOptions = {
+  //     key: fs.readFileSync(PRIVATE_KEY_PATH),
+  //     cert: fs.readFileSync(PUBLIC_CERTIFICATE_PATH),
+  //   };
+  // }
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   const allowedOrigins = process.env.CORS_ORIGIN
